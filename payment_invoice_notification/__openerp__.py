0729f4ce5b4c132e5df5a6ab04dd47d939c8d003
Van Driessche Jérémy <jeremy@niboo.be>
2016-02-25 10:57:57 +0100

    NIB-40 : Add new state in suppliers invoices workflow

---
+++ payment_invoice_notification/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Payment Invoice Notification',
+    'category': 'Payment',
+    'summary': 'Add a notification on the invoice if it is already paid',
+    'website': '',
+    'version': '1.0',
+    'description': """
+Add a state on the invoice between "open" and "paid"
+It will notify that the invoice is already paid but not reconcilied.
+        """,
+    'author': 'Niboo',
+    'depends': ['account','payment'],
+    'data': [
+        'data/invoice_wkf.xml',
+        'views/invoice_view.xml',
+        'views/report_invoice.xml',
+    ],
+    'qweb': [],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
+
