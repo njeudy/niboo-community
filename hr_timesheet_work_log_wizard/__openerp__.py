a7e8ad3358099efd58afff9c57edaea05f2efc01
Tobias Zehntner <tobias@niboo.be>
2016-09-23 16:55:36 +0200

    NIB-80: Add work_log_wizard

---
+++ hr_timesheet_work_log_wizard/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Pierre Faniel
+#    Copyright 2016 Niboo SPRL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Log work on timesheet with wizard',
+    'category': 'HR',
+    'summary': 'Log work on timesheet with wizard',
+    'website': '',
+    'version': '1.0',
+    'description': """
+        This module allows logging work efficently on timesheets with a wizard.
+        The user launches the wizard with a button at the top of the timesheet
+        and can define project, task, product, date and hours.
+        """,
+    'author': 'Niboo',
+    'depends': [
+        'hr_timesheet',
+        'hr_timesheet_task',
+    ],
+    'data': [
+        'views/hr_timesheet.xml',
+    ],
+    'qweb': [],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ hr_timesheet_work_log_wizard/static/src/js/timesheet.js
@@ -0,0 +1,80 @@
+odoo.define('readonly_sheet.sheet', function (require) {
+"use strict";
+
+var core = require('web.core');
+var data = require('web.data');
+var form_common = require('web.form_common');
+var formats = require('web.formats');
+var Model = require('web.DataModel');
+var time = require('web.time');
+var utils = require('web.utils');
+var sheet = require('hr_timesheet_sheet.sheet');
+
+var QWeb = core.qweb;
+var _t = core._t;
+
+var WeeklyTimesheet = core.form_custom_registry.get('weekly_timesheet').extend({
+        events: {
+        "click .oe_timesheet_weekly_account a": "go_to",
+        "click .oe_timesheet_weekly_account_task a": "go_to_task",
+    },
+
+    initialize_content: function() {
+        var self = this;
+        self.set('effective_readonly', true);
+        self._super();
+    },
+
+    display_data: function() {
+        var self = this;
+        self._super();
+        self.add_listener_on_oe_timesheet_weekly_box();
+    },
+    add_listener_on_oe_timesheet_weekly_box: function(){
+        var self = this;
+
+        var on_closed = function(reason) {
+            if (!_.isObject(reason)) {
+                self.view.recursive_reload();
+            }
+        }
+
+        var result_handler = function () {
+            if (on_closed) { on_closed.apply(null, arguments); }
+            if (self.getParent() && self.getParent().on_action_executed) {
+                return self.getParent().on_action_executed.apply(null, arguments);
+            }
+        };
+
+        $(".oe_timesheet_weekly_box:parent").off('click.box');
+        $(".oe_timesheet_weekly_box:parent").on('click.box', function (event) {
+            var project_and_task = $(event.currentTarget).attr('data-account-task').split(',');
+            var project_id = project_and_task[0];
+            var task_id = project_and_task[1];
+
+            var days_offest = $(event.currentTarget).attr('data-day-count');
+            var date_start = self.field_manager.get_field_value("date_from");
+            var millis = new Date(date_start).getTime();
+            millis += days_offest * 24 * 60 * 60 *1000;
+            var true_date = new Date(millis);
+
+            self.do_action({
+                name: _t("Log Work"),
+                type: "ir.actions.act_window",
+                res_model: "hr_timesheet.work.logger",
+                domain : [],
+                views: [[false, "form"]],
+                target: 'new',
+                context: {
+                    'default_analytic_account_id':parseInt(project_id),
+                    'default_task_id':parseInt(task_id),
+                    'default_date_started':true_date,
+                },
+            },{on_close: result_handler});
+        });
+    }
+});
+
+core.form_custom_registry.add('weekly_timesheet', WeeklyTimesheet);
+
+});
