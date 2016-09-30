238167c22ab644b01c4056e95f0a652297ddbb5d
Pierre Faniel <pierre@niboo.be>
2016-09-30 16:40:56 +0200

    NIB-97: Link module between project_version and project_workflow

---
+++ project_version_workflow/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Pierre Faniel
+# © 2016 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Project - Version and Workflow link',
+    'category': 'Project',
+    'summary': 'Allows to change task step on version state change',
+    'website': 'https://www.niboo.be/',
+    'version': '9.0.1.0.0',
+    'description': '''
+Allows to change task step on version state change.
+    ''',
+    'author': 'Niboo',
+    'depends': [
+        'project_version',
+        'project_workflow',
+    ],
+    'data': [
+        'views/version_state_step_link.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ project_version_workflow/models/project.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# © 2016 Pierre Faniel
+# © 2016 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class VersionStateStepLink(models.Model):
+    _name = 'version.state.step.link'
+
+    state_id = fields.Many2one('project.version.state', 'Version State',
+                               required=True)
+    step_id = fields.Many2one('project.task.step', 'Task Step',
+                              required=True)
+    category_id = fields.Many2one('project.task.category', 'Task Type',
+                                  required=True)
+
+    @api.onchange('category_id')
+    def _onchange_category_id(self):
+        domain = {'step_id': []}
+
+        # Remove the step if it is not matching with the workflow
+        if self.step_id and self.category_id \
+            and self.category_id.workflow_id.id \
+                        not in self.step_id.workflow_ids.ids:
+            self.step_id = False
+
+        if self.category_id:
+            domain['step_id'] = [(
+                'workflow_ids', 'in', self.category_id.workflow_id.id)]
+        return {'domain': domain}
+
+    _sql_constraints = [(
+        'state_category_unique',
+        'unique (state_id,category_id)',
+        'There is already an action on this state for this type of task'
+    )]
+
+
+class ProjectVersion(models.Model):
+    _inherit = 'project.version'
+
+    @api.multi
+    @api.constrains('state_id', 'task_ids')
+    def check_state(self):
+        super(ProjectVersion, self).check_state()
+        ProjectLink = self.env['version.state.step.link']
+        for version in self:
+            links = ProjectLink.search([
+                ('state_id', '=', version.state_id.id),
+            ])
+            for link in links:
+                tasks = version.task_ids.filtered(
+                    lambda task: task.category_id.id == link.category_id.id)
+                if tasks:
+                    tasks.write({'step_id': link.step_id.id})
