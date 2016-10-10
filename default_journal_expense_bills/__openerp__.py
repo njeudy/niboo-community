4d3893387bc1bc7af79c302a291c8f5a5aaf51f3
Tobias Zehntner <tobias@niboo.be>
2016-10-10 14:09:51 +0200

    NIB-88: add description files

--- default_journal_expense_bills/__openerp__.py
+++ default_journal_expense_bills/__openerp__.py
@@ -4,11 +4,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Default Journal for Expenses and Vendor Bills',
+    'name': 'Account - Default Journal for Expense and Vendor Bill',
     'category': 'Accounting & Finance',
     'summary': 'Automatically select the proper journal for expenses and vendor bills.',
-    'website': '',
-    'version': '9.1.0',
+    'website': 'https://odoo-community.org/',
+    'license': 'AGPL-3',
+    'version': '9.0.1.0.0',
     'description': '''
         This module selects automatically the proper journal for expenses
         and vendor bills after they have been defined in the accounting
@@ -24,12 +25,12 @@
         - Type: Purchase
         - Subtype: Expenses
 
-        The one that has 'Vendor Bills' or 'Expenses' selected will be set as
+        The journal that has 'Vendor Bills' or 'Expenses' selected will be set as
         default when creating the corresponding new records. Please note that
         the journal type must be set to 'Purchase'.
 
         ''',
-    'author': 'Niboo',
+    'author': 'Niboo, Odoo Community Association (OCA)',
     'depends': [
         'hr_expense',
         'account'
@@ -37,12 +38,6 @@
     'data': [
         'views/account_journal.xml',
     ],
-    'qweb': [
-    ],
-    'demo': [
-    ],
-    'css': [
-    ],
     'installable': True,
-    'application': True,
+    'application': False,
 }
