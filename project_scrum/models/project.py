1c54226e26cdcc2ad9fe5e2a7ba3847936cf1b1c
Pierre Faniel <pierre@niboo.be>
2016-10-13 16:58:24 +0200

    NIB-94: Fixed disabled create in 'View Tasks' and desc improvement

--- project_scrum/models/project.py
+++ project_scrum/models/project.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Pierre Faniel
-#    Copyright 2016 Niboo SPRL
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import api, fields, models
-from openerp import exceptions
-from lxml import etree
+# © 2016 Pierre Faniel
+# © 2016 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, exceptions, fields, models
+
 
 class ProjectScrumTeam(models.Model):
     _name = 'project.scrum.team'
@@ -118,17 +102,18 @@ class ProjectSprint(models.Model):
             'type': 'ir.actions.act_window',
             'res_model': 'project.task',
             'view_type': 'form',
-            'view_mode': 'tree',
+            'view_mode': 'tree,form',
             'target': 'current',
             'name': self.name,
             'display_name': self.display_name,
             'domain': [('sprint_id', '=', self.id)],
+            'context': {'default_sprint_id': self.id,}
         }
 
     @api.depends('name', 'start_date', 'end_date')
     def _compute_display_name(self):
         for sprint in self:
-            sprint.display_name = "%s - %s/%s" % (sprint.name,
+            sprint.display_name = '%s - %s/%s' % (sprint.name,
                                                   sprint.end_date[8:10],
                                                   sprint.end_date[5:7])
 
