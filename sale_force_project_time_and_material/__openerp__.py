c4d4d8045daafd4435d92fbe21b015d14809d93c
Tobias Zehntner <tobias@niboo.be>
2016-10-14 15:06:09 +0200

    NIB-85: Add cover and icon

--- sale_force_project_time_and_material/__openerp__.py
+++ sale_force_project_time_and_material/__openerp__.py
@@ -6,23 +6,20 @@
 {
     'name': 'Force selection of project on sale order',
     'category': 'Sales Management',
-    'summary': 'Force selection of project on sale order for time and material products',
-    'website': 'http://www.niboo.be',
-    'version': '1.0',
+    'summary': 'Force selection of project on sale order containing time and material products',
+    'website': 'https://www.niboo.be/',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
     'description': """
         On a Sale Order, this module will make the field 'Project' a requirement
-        if it contains a product which invoice policy is based on time and
+        if the order contains a product which invoice policy is based on time and
         material. This applies only if the project field is visible, i.e.
         the user has the group 'Analytic Accounting'.
         """,
     'author': 'Niboo',
     'depends': ['sale'],
-    'data': [
-    ],
-    'qweb': [],
-    'demo': [
-    ],
-    'css': [
+    'images': [
+        'static/description/sale_force_project_time_and_material_cover.png',
     ],
     'installable': True,
     'application': False,
