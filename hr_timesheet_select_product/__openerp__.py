49c60c5b040bfbb83237f37167a4153456d8a0d2
Tobias Zehntner <tobias@niboo.be>
2016-10-17 13:40:34 +0200

    NIB-89: add description, cover, icon

--- hr_timesheet_select_product/__openerp__.py
+++ hr_timesheet_select_product/__openerp__.py
@@ -1,42 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Pierre Faniel
-#    Copyright 2016 Niboo SPRL
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Pierre Faniel
+# © 2016 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Select product on timesheet',
-    'category': "Generic Modules/Others",
-    'summary': 'Select multiple products on timesheet',
-    'website': '',
-    'version': '1.0',
-    'description': """.
-        """,
+    'name': 'HR Timesheet - Select product',
+    'category': 'HR',
+    'summary': 'Select product for each timesheet entry',
+    'website': 'https://www.niboo.be/',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'description': '''
+With this module you are able to choose a product for each timesheet entry.
+        ''',
     'author': 'Niboo',
     'depends': ['sale_timesheet', 'project_timesheet', 'hr_timesheet_sheet'],
     'data': [
         'views/project_task.xml',
     ],
-    'qweb': [],
-    'demo': [
-    ],
-    'css': [
+    'images': [
+        'static/description/hr_timesheet_select_product_cover.png',
     ],
     'installable': True,
-    'application': True,
+    'application': False,
 }
