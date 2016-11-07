046d6c27873018f53244bf9b0b32dfb6cab4a689
Pierre Faniel <pierre@niboo.be>
2016-11-07 16:15:18 +0100

    NIB-115: Update account_analytic_distribution_plan description

--- account_analytic_distribution_plan/__openerp__.py
+++ account_analytic_distribution_plan/__openerp__.py
@@ -1,31 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Samuel Lefever
+# © 2015 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Multiple Analytic Distribution Plan',
-    'version': '1.0',
+    'version': '9.0.1.0.0',
     'summary': 'Manage analytic accounting through multiple universe with the possibility to prepare distribution to reuse',
     'category': 'Accounting & Finance',
     'description': """
-
+This module allows you to prepare plan for recurring distribution (like salary)
         """,
     'author': 'Niboo',
     'website': 'https://www.niboo.be',
@@ -42,13 +26,8 @@
         'wizard/account_reconcile_writeoff_view.xml',
     ],
     'images': [
-        'static/description/'
-        'account_analytic_distribution_plan_cover.png',
+        'static/description/cover.png',
     ],
-    'demo': [],
-    'test': [],
     'installable': True,
     'auto_install': False,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
