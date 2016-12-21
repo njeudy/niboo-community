9e88ebf427db252fceda2136c1f395975b67ac4b
Pierre Faniel <pierre@niboo.be>
2016-12-21 10:14:08 +0100

    NIB-178: Clean sale_order_to_subscription for community

---
+++ sale_order_to_subscription/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2015 Gael Rabier
+# © 2015 Niboo SPRL (<https://www.niboo.be/>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import sale_order
+from . import sale_subscription
+from . import account_invoice
